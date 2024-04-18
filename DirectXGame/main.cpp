b6d73e3a801a5cd9ca6559207747abc597d01ec4
LE2C_16_タカキ_ケンゴ <146917575+Guitnggg@users.noreply.github.com>
2024-04-18 12:04:09 +0900

    変更

--- DirectXGame/main.cpp
+++ DirectXGame/main.cpp
@@ -20,7 +20,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
 	// ゲームウィンドウの作成
 	win = WinApp::GetInstance();
-	win->CreateGameWindow(L"LE2C_16_タカキ_ケンゴ_タイトル");
+	win->CreateGameWindow(L"LE2C_16_タカキ_ケンゴ_AL3");
 
 	// DirectX初期化処理
 	dxCommon = DirectXCommon::GetInstance();
