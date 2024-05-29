3c0298329672aaa82ae41b3cd296d09c53d3f40e
LE2C_16_タカキ_ケンゴ <146917575+Guitnggg@users.noreply.github.com>
2024-05-29 11:51:37 +0900

    途中

--- DirectXGame/scene/GameScene.h
+++ DirectXGame/scene/GameScene.h
@@ -4,12 +4,16 @@
 #include "DirectXCommon.h"
 #include "Input.h"
 #include "Model.h"
-#include "SafeDelete.h"
 #include "Sprite.h"
 #include "ViewProjection.h"
 #include "WorldTransform.h"
-#include <vector>
+#include "Player.h"
 #include "DebugCamera.h"
+#include "Skydome.h"
+
+#include <vector>
+
+
 
 /// <summary>
 /// ゲームシーン
@@ -46,24 +50,33 @@ private: // メンバ変数
 	DirectXCommon* dxCommon_ = nullptr;
 	Input* input_ = nullptr;
 	Audio* audio_ = nullptr;
-	
-	// 3Dモデルデータ
-	Model* model_ = nullptr;
-	Model* modelBlock_ = nullptr;
-
-	// 
-	ViewProjection viewProjection_;
-
-	// 縦横ブロック配列
-	std::vector<std::vector<WorldTransform*>> worldTransformBlocks_;
-
-	// デバックカメラ有効
-	bool isDebugCameraActive_ = false;
-
-	// デバックカメラ
-	DebugCamera* debugCamera_ = nullptr;
 
 	/// <summary>
 	/// ゲームシーン用
 	/// </summary>
+	// テクスチャハンドル
+	uint32_t textureHandle_ = 0;
+	// 3Dモデル
+	Model* model_ = nullptr;
+	Model* modelBlock_ = nullptr;
+	// ワールドトランスフォーム
+	WorldTransform worldTransform_;
+	// ビュープロジェクション
+	ViewProjection viewProjection_;
+
+	// 自キャラ
+	Player* player_ = nullptr;
+
+	// 縦横ブロック配列
+	std::vector<std::vector<WorldTransform*>> worldTransformBlocks_;
+
+	// デバッグカメラ有効
+	bool isDebugCameraActive_ = false;
+	// デバッグカメラ
+	DebugCamera* debugCamera_ = nullptr;
+
+	//天球
+	Skydome* skydome_ = nullptr;
+	// 3Dモデル
+	Model* modelSkydome_ = nullptr;
 };
