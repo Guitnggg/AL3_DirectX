ef53c83df7bf0882a1e8da81485b6d68c9fea08c
LE2C_16_タカキ_ケンゴ <146917575+Guitnggg@users.noreply.github.com>
2024-05-30 12:15:12 +0900

    totyuu

--- DirectXGame/scene/GameScene.cpp
+++ DirectXGame/scene/GameScene.cpp
@@ -28,7 +28,7 @@ void GameScene::Initialize() {
 	audio_ = Audio::GetInstance();
 
 	// ファイル名を指定してテクスチャを読み込む
-	textureHandle_ = TextureManager::Load("Resources/cube/cube.jpg");
+	/*textureHandle_ = TextureManager::Load("block.jpg");*/
 	// 3Dモデルの生成
 	model_ = Model::Create();
 	modelBlock_ = Model::Create();
