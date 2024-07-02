8179394d06229bb04b0340a4dfebadf68f11b938
LE2C_16_タカキ_ケンゴ <146917575+Guitnggg@users.noreply.github.com>
2024-07-02 12:29:56 +0900

    a

--- DirectXGame/Player.cpp
+++ DirectXGame/Player.cpp
@@ -1,1 +1,25 @@
 #include "Player.h"
+#include <cassert>
+
+void Player::Inttialize(Model* model, uint32_t textureHandle, ViewProjection* viewProjection)
+{
+	// NULLポインタチェック
+	assert(model);
+
+	model_ = model;
+	textureHandle_ = textureHandle;
+	viewProjection_ = viewProjection;
+
+	worldTransform_.Initialize();
+}
+
+void Player::Update()
+{
+	// 行列を定数バッファに転送
+	worldTransform_.TransferMatrix();
+}
+
+void Player::Draw()
+{
+	model_->Draw(worldTransform_, *viewProjection_, textureHandle_);
+}
