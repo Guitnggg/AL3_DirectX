c3954d79db8ac178311d340e486054d17a204904
LE2C_16_タカキ_ケンゴ <146917575+Guitnggg@users.noreply.github.com>
2024-07-02 12:29:09 +0900

    a

--- DirectXGame/Player.h
+++ DirectXGame/Player.h
@@ -1,5 +1,34 @@
-#pragma once
-class Player
-{
-};
+#include "Model.h"
+#include "WorldTransform.h"
 
+class Player {
+public:
+	/// <summary>
+	/// 初期化
+	/// </summary>
+	/// <param name="model">モデル</param>
+	/// <param name="textureHandle">テクスチャハンドル</param>
+	void Inttialize(Model* model, uint32_t textureHandle, ViewProjection* viewProjection);
+
+	/// <summary>
+	/// 初期化
+	/// </summary>
+	void Update();
+
+	/// <summary>
+	/// 初期化
+	/// </summary>
+	void Draw();
+
+private:
+	// ワールド変換データ
+	WorldTransform worldTransform_;
+
+	// モデル
+	Model* model_ = nullptr;
+
+	// テクスチャハンドル
+	uint32_t textureHandle_ = 0u;
+
+	ViewProjection* viewProjection_ = nullptr;
+};
